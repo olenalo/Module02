b9899b3b326117a208801e7a8486d612a8c2f1ce
olenalo <uleta@ukr.net>
2018-11-23 04:12:41 +0200

    Remove PoC code

--- src/com/company/Configs.java
+++ src/com/company/Configs.java
@@ -3,7 +3,9 @@ package com.company;
 public class Configs {
     public static final int BYTES_MAX_NUMBER = 256;
     public static final int EIGHT_BITS = 8;
+    public static final String COMPRESSED_FILE_EXTENSION = ".hf";
+
+    // This should probably be provided by a user; presented here to speed up debugging
     public static final String METADATA_TABLE_FILENAME = "metadata.table.txt";
     public static final String COMPRESSED_FILENAME = "compressed.hf";
-    public static final String COMPRESSED_FILE_EXTENSION = ".hf";
 }
